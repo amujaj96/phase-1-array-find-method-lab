5c53b074a8811cc1db1ddc314954b2bf377debb7
Agim <dawnhillsnyc@gmail.com>
2022-10-08 21:26:14 -0400

    commit tree 6dc4c17c94aaa97c587bb5c0b7711bc35ee05f07

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function callBack(record){
+    return record.result === "W"
+  }
+ 
+  function superbowlWin(record) {
+    const findRecord = record.find(callBack)
+    console.log(findRecord)
+    if(findRecord == undefined){
+    return findRecord}
+    else {
+        return findRecord.year
+    }
+}
